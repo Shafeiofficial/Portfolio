b0d51a03a4a11df07bbe3a25b6b77d3a3c95e644
Shafeiofficial <Shafeiofficial@gmail.com>
2024-07-14 18:47:21 +0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function filterContent() {
+    var artist = document.getElementById('artist').value;
+    var genre = document.getElementById('genre').value;
+    // Add more filters as needed
+
+    // Logic to filter content based on selected filters
+    // This requires additional implementation to load and filter content dynamically
+}
